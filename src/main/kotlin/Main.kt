7633277909f9d48962c9637be8464a4be7624f05
Lol-ice <lolice@mail.ru>
2022-06-21 11:45:26 +0300

    2 commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,6 @@
-fun  main() {
+fun main() {
+    val likes = 1511
+    val people = if (likes == 1 || (likes % 100 != 11 && likes % 10 == 1)) "человеку" else "людям"
 
-    var likes = 11 //поставленные "лайки"
-    if (likes == 1 || likes != 11 && likes % 2 == 1 && (likes + 1) % 2 == 1 || likes % 10 == 1 && likes != 11) {
-        println("Понравилось  " + likes + " человеку")
-    } else if (likes == 11 || (likes + 1) % 2 == 0 && likes % 2 == 1 || likes % 2 == 0 && likes != 0) {
-        println("Понравилось " + likes + " людям")
-    } else {
-        println("Никому не понравилось")
-    }
-
+    println("Понравилось $likes $people")
 }
